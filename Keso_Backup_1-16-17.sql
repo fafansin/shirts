ab27c6fa40f1020ff8844bbaf74789b51b8a85ba
fafansin <administrator@Administrators-MacBook-Pro-3.local>
2017-01-16 10:26:34 +0300

    Add backup for Keso DB

---
+++ Keso_Backup_1-16-17.sql
@@ -0,0 +1,83 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.5.38)
+# Database: keso
+# Generation Time: 2017-01-16 07:19:53 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table user_types
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_types`;
+
+CREATE TABLE `user_types` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `user_types` WRITE;
+/*!40000 ALTER TABLE `user_types` DISABLE KEYS */;
+
+INSERT INTO `user_types` (`id`, `name`)
+VALUES
+	(1,'admin'),
+	(2,'encoder'),
+	(3,'sales'),
+	(4,'customer');
+
+/*!40000 ALTER TABLE `user_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `first_name` text,
+  `last_name` text,
+  `password` text NOT NULL,
+  `email` text NOT NULL,
+  `usertype` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `password`, `email`, `usertype`)
+VALUES
+	(1,'laviel','Laviel','Dela Cruz','$2y$10$smqkhVsDU0htmrs/nYZ2W.cyIr/sa92zlE5BVK3ruThzmRkSWUq.O','fafansin@gmail.com',1),
+	(2,'rivka','Rivka Angela','Dela Cruz','$2y$10$NDj6gCCLcieSeHzOnEVAUOb4afoViPx8GeSaHq2WvSWvO4Yt34CMq','rivka@hotmail.com',1);
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
